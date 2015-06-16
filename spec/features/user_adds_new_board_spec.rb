5fbe1b6f507ae017fb2286c170b3bca7167dd223
Miriam Moser <mirjoy.moser@gmail.com>
2015-06-16 10:32:07 -0600

    flash messages now displaying

--- spec/features/user_adds_new_board_spec.rb
+++ spec/features/user_adds_new_board_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe "user can add a new board", type: :feature do
     visit new_project_path
     fill_in "project[title]", with: "Oscar's new trashcan"
     click_link_or_button("Save Project")
+
+    expect(page).to have_content("You have successfully created a new board")
     expect(page).to have_content("Project Status")
     expect(page).to have_content("Oscar's new trashcan")
   end
