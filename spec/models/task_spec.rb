50119750ab9984baa61ac014836cf8d509805c20
Miriam Moser <mirjoy.moser@gmail.com>
2015-06-16 15:23:46 -0600

    added title

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
  it "must have a title to be created" do
-    p = Project.create("hi")
+    p = Project.create(title: "hi")
     t1 = Task.create(title: "make toast", project_id: 1)
     t2 = Task.create(title: "")
 
